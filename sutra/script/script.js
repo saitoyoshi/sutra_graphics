53c97ee60b7ead85cb1325165c316b61621bb8e0
saitoyoshi <mitti180@gmail.com>
2023-03-04 10:01:01 +0900

    矢印キーで自分機を動かせるように変更

--- sutra/script/script.js
+++ sutra/script/script.js
@@ -4,12 +4,12 @@
      * canvasの幅
      * @type {number}
      */
-    const CANVAS_WIDTH = 320;
+    const CANVAS_WIDTH = 400;
     /**
      * canvasの高さ
      * @type {number}
      */
-    const CANVAS_HEIGHT = 340;
+    const CANVAS_HEIGHT = 620;
 
     /**
      * Canvas2D APIをラップしたユーティリティクラス
@@ -37,6 +37,16 @@
     // 実行開始時のタイスタンプ
     let startTime = null;
     /**
+     * viper x position
+     *
+     */
+    let viperX = CANVAS_WIDTH / 2;
+    /**
+     * viper y position
+     *
+     */
+    let viperY = CANVAS_HEIGHT / 2;
+    /**
      * リソースがすべて読み込まれたら実行されるアクション
      */
     window.addEventListener('load',
@@ -52,10 +62,11 @@
         util.imageLoader('./image/viper.png', loadedImage => {
             // 引数経由で画像を受け取り変数に入れておく
 
-
             image = loadedImage;
             //
             initialize();
+            //
+            eventSetting();
             //
             startTime = Date.now();//now()はDateオブジェクトの静的メソッド
             //
@@ -73,7 +84,25 @@
         canvas.window = CANVAS_WIDTH;
         canvas.height = CANVAS_HEIGHT;
     }
-
+    function eventSetting() {
+        // キーが押されたときに、実行したいアクションを登録
+        window.addEventListener('keydown', e => {
+            switch (e.key) {
+                case 'ArrowLeft':
+                    viperX -= 10;
+                    break;
+                case 'ArrowRight':
+                    viperX += 10;
+                    break;
+                case 'ArrowUp':
+                    viperY -= 10;
+                    break;
+                case 'ArrowDown':
+                    viperY += 10;
+                    break;
+            }
+        },false);
+    }
     /**
      *
      */
@@ -82,14 +111,8 @@
         util.drawRect(0, 0, canvas.width, canvas.height, 'black');
         // 現在までの経過時間を取得
         let nowTime = (Date.now() - startTime) / 1000;
-        // 時間の経過を見るために、自分機をサイン関数で動かす
-        let s = Math.sin(nowTime);
-        //
-        //
-        let x = s * 30;
 
-        // canvasの中心からサイン関数で左右に揺らす
-        ctx.drawImage(image,CANVAS_WIDTH / 2 + x, CANVAS_HEIGHT /2);
+        ctx.drawImage(image,viperX, viperY);
 
         // ずっとループさせつづけるために、描画処理を再帰呼び出し
         requestAnimationFrame(render);

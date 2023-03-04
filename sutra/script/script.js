ab5d5b5aa939ac59bdb0fa1a770b07cc0ec3d1f7
saitoyoshi <mitti180@gmail.com>
2023-03-04 09:41:00 +0900

    時間の経過を導入

--- sutra/script/script.js
+++ sutra/script/script.js
@@ -4,12 +4,12 @@
      * canvasの幅
      * @type {number}
      */
-    const CANVAS_WIDTH = 480;
+    const CANVAS_WIDTH = 320;
     /**
      * canvasの高さ
      * @type {number}
      */
-    const CANVAS_HEIGHT = 320;
+    const CANVAS_HEIGHT = 340;
 
     /**
      * Canvas2D APIをラップしたユーティリティクラス
@@ -23,9 +23,19 @@
     let ctx = null;
     /**
      * 画像のインスタンス
+     *
      */
-    let image = null;
 
+
+
+
+    let image = null;
+    /**
+     *
+     *
+     */
+    // 実行開始時のタイスタンプ
+    let startTime = null;
     /**
      * リソースがすべて読み込まれたら実行されるアクション
      */
@@ -41,9 +51,13 @@
         // まず最初に画像の読み込みを開始
         util.imageLoader('./image/viper.png', loadedImage => {
             // 引数経由で画像を受け取り変数に入れておく
+
+
             image = loadedImage;
             //
             initialize();
+            //
+            startTime = Date.now();//now()はDateオブジェクトの静的メソッド
             //
             render();
         });
@@ -66,8 +80,19 @@
     function render() {
         //
         util.drawRect(0, 0, canvas.width, canvas.height, 'black');
+        // 現在までの経過時間を取得
+        let nowTime = (Date.now() - startTime) / 1000;
+        // 時間の経過を見るために、自分機をサイン関数で動かす
+        let s = Math.sin(nowTime);
         //
-        ctx.drawImage(image, 100, 100);
+        //
+        let x = s * 30;
+
+        // canvasの中心からサイン関数で左右に揺らす
+        ctx.drawImage(image,CANVAS_WIDTH / 2 + x, CANVAS_HEIGHT /2);
+
+        // ずっとループさせつづけるために、描画処理を再帰呼び出し
+        requestAnimationFrame(render);
     }
 
     /**

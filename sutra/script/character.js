267a9fc154af819dde01b22a2827aa007ea728ec
saitoyoshi <mitti180@gmail.com>
2023-03-05 09:21:24 +0900

    矢印キーでのviperの移動をメインプログラムから分離007

--- sutra/script/character.js
+++ sutra/script/character.js
@@ -2,187 +2,214 @@
  * 座標概念を使用するためのクラス
  */
 class Position {
+  /**
+   *
+   *
+   *
+   */
+  constructor(x, y) {
     /**
      *
      *
+     */
+    this.x = x;
+    /**
+     *
      *
      */
-    constructor(x, y) {
-        /**
-         *
-         *
-         */
-        this.x = x;
-        /**
-         *
-         *
-         */
-        this.y = y;
-    }
+    this.y = y;
+  }
 
-    /**
-     * 座標を再設定する
-     *
-     *
-     */
-    set(x,y) {
-        if (x != null) {
-            this.x = x;
-        }
-        if (y != null) {
-            this.y = y;
-        }
+  /**
+   * 座標を再設定する
+   *
+   *
+   */
+  set(x, y) {
+    if (x != null) {
+      this.x = x;
     }
+    if (y != null) {
+      this.y = y;
+    }
+  }
 }
 
 /**
  * 画面上のオブジェクト（キャラクター）を使うためのクラス、このクラスがベースとなる
  */
 class Character {
+  /**
+   *
+   *
+   *
+   *
+   *
+   * imageはオブジェクトの画像
+   */
+  constructor(ctx, x, y, w, h, life, image) {
+    /**
+     *
+     */
+    this.ctx = ctx;
+    /**
+     * 画面上のオブジェクトの存在する座標
+     */
+    this.position = new Position(x, y);
+    /**
+     *
+     */
+    this.width = w;
     /**
      *
      *
-     *
-     *
-     *
-     * imageはオブジェクトの画像
      */
-    constructor(ctx, x, y, w, h, life, image) {
-      /**
-       *
-       */
-      this.ctx = ctx;
-      /**
-       * 画面上のオブジェクトの存在する座標
-       */
-      this.position = new Position(x, y);
-      /**
-       *
-       */
-      this.width = w;
-      /**
-       *
-       *
-       */
-      this.height = h;
-      /**
-       *
-       *
-       */
-      this.life = life;
-      /**
-       *
-       */
-      this.image = image;
-    }
+    this.height = h;
+    /**
+     *
+     *
+     */
+    this.life = life;
+    /**
+     *
+     */
+    this.image = image;
+  }
 
-    /**
-     * オブジェクトを画面に描画する
-     */
-    draw() {
-        // offset
-        let offsetX = this.width / 2;
-        let offsetY = this.height / 2;
-        this.ctx.drawImage(
-            this.image,
-            this.position.x - offsetX,
-            this.position.y - offsetY,
-            this.width,
-            this.height
-        );
-    }
+  /**
+   * オブジェクトを画面に描画する
+   */
+  draw() {
+    // offset
+    let offsetX = this.width / 2;
+    let offsetY = this.height / 2;
+    this.ctx.drawImage(
+      this.image,
+      this.position.x - offsetX,
+      this.position.y - offsetY,
+      this.width,
+      this.height
+    );
+  }
 }
 
 /**
  * 自分機クラス viperはシューティングゲームの主人公としての山田太郎的な名前である
  */
 class Viper extends Character {
+  /**
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   */
+  constructor(ctx, x, y, w, h, image) {
+    //
+    // インスタンス化したときには、ライフは画面に描画されないためのフラグとして,0を与える
+    super(ctx, x, y, w, h, 0, image);
+
+    /**
+     * viperが更新されるたびに移動する量(スピードという概念と論理的には等価)
+     *
+     */
+    this.speed = 3;
     /**
      *
      *
+     */
+    this.isComing = false;
+    /**
      *
-     *
-     *
-     *
+     * 登場開始シーンが始まったときのタイムスタンプ
+     */
+    this.comingStart = null;
+    /**
+     * 登場シーンを開始する座標
      *
      */
-    constructor(ctx, x, y, w, h, image) {
-        //
-        //
-        // インスタンス化したときには、ライフは画面に描画されないためのフラグとして,0を与える
-        super(ctx, x, y, w, h, 0, image);
+    this.comingStartPosition = null;
+    /**
+     *
+     * 登場シーンが終わるときにviperの座標
+     */
+    this.comingEndPosition = null;
+  }
 
-        /**
-         *
-         *
-         */
+  /**
+   * 登場シーンに関する設定を行う
+   *
+   *
+   *
+   *
+   */
+  setComing(startX, startY, endX, endY) {
+    //
+    this.isComing = true;
+    //
+    this.comingStart = Date.now();
+    //
+    this.position.set(startX, startY);
+    //
+    this.comingStartPosition = new Position(startX, startY);
+    //
+    this.comingEndPosition = new Position(endX, endY);
+  }
+
+  /**
+   * 画面オブジェクトを更新したのちに描画する
+   */
+  update() {
+    // 今の時点でのタイムスタンプを取得
+    let justTime = Date.now();
+
+    // 登場シーンであるか、そうでないかで更新処理を分岐させる
+    if (this.isComing === true) {
+      // 登場シーンがスタートしてからの経過時間
+      let comingTime = (justTime - this.comingStart) / 1000;
+      //
+      let y = this.comingStartPosition.y - comingTime * 50;
+      //
+      if (y <= this.comingEndPosition.y) {
         this.isComing = false;
-        /**
-         *
-         * 登場開始シーンが始まったときのタイムスタンプ
-         */
-        this.comingStart = null;
-        /**
-         * 登場シーンを開始する座標
-         *
-         */
-        this.comingStartPosition = null;
-        /**
-         *
-         * 登場シーンが終わるときにviperの座標
-         */
-        this.comingEndPosition = null;
+        // 行き過ぎる可能性があるので補正する
+        y = this.comingEndPosition.y;
+      }
+      // viperの上昇を実現するために、計算したy座標を設定する
+      this.position.set(this.position.x, y);
+
+      if (justTime % 4 === 0) {
+        this.ctx.globalAlpha = 0.5; //半透明にして、点滅を表現する
+      }
+    } else {
+      // キーが押された状態に応じて座標を変更する
+      if (window.isKeyDown.key_ArrowLeft === true) {
+        this.position.x -= this.speed;
+      }
+      if (window.isKeyDown.key_ArrowRight === true) {
+        this.position.x += this.speed;
+      }
+      if (window.isKeyDown.key_ArrowUp === true) {
+        this.position.y -= this.speed;
+      }
+      if (window.isKeyDown.key_ArrowDown === true) {
+        this.position.y += this.speed;
+      }
+      // 移動後の位置が画面がでてしまっていたら場合には、画面端で泊まるようにする
+      let canvasWidth = this.ctx.canvas.width;
+      let canvasHeight = this.ctx.canvas.height;
+      // x座標が0より大きくcanvasWidthより小さければいいので
+      let tx = Math.min(Math.max(this.position.x, 0), canvasWidth);
+      let ty = Math.min(Math.max(this.position.y, 0), canvasHeight);
+      // 目的を実現するための計算処理で求めた座標をセットする
+      this.position.set(tx, ty);
     }
+    // 計算した、y座標、点滅の適用のもとで、viperを画面に描画する
+    this.draw();
 
-    /**
-     * 登場シーンに関する設定を行う
-     *
-     *
-     *
-     *
-     */
-    setComing(startX, startY, endX, endY) {
-        //
-        this.isComing = true;
-        //
-        this.comingStart = Date.now();
-        //
-        this.position.set(startX, startY);
-        //
-        this.comingStartPosition = new Position(startX, startY);
-        //
-        this.comingEndPosition = new Position(endX, endY);
-    }
-    /**
-     * 画面オブジェクトを更新したのちに描画する
-     */
-    update() {
-        // 今の時点でのタイムスタンプを取得
-        let justTime = Date.now();
-
-        // 登場シーンのときの処理
-        if (this.isComing === true) {
-            // 登場シーンがスタートしてからの経過時間
-            let comingTime = (justTime - this.comingStart) / 1000;
-            //
-            let y = this.comingStartPosition.y - comingTime * 50;
-            //
-            if (y <= this.comingEndPosition.y) {
-                this.isComing = false;
-                // 行き過ぎる可能性があるので補正する
-                y = this.comingEndPosition.y;
-            }
-            // viperの上昇を実現するために、計算したy座標を設定する
-            this.position.set(this.position.x, y);
-
-            if (justTime % 4 === 0) {
-                this.ctx.globalAlpha = 0.5;//半透明にして、点滅を表現する
-            }
-        }
-        // 計算した、y座標、点滅の適用のもとで、viperを画面に描画する
-        this.draw();
-
-        // 念のためににグローバルアルファを戻しておく
-        this.ctx.globalAlpha = 1.0;
-    }
+    // 念のためににグローバルアルファを戻しておく
+    this.ctx.globalAlpha = 1.0;
+  }
 }
--- sutra/script/script.js
+++ sutra/script/script.js
@@ -1,150 +1,144 @@
 // 即時関数で変数スコープを限定
 (() => {
-    /**
-     * canvasの幅
-     * @type {number}
-     */
-    const CANVAS_WIDTH = 400;
-    /**
-     * canvasの高さ
-     * @type {number}
-     */
-    const CANVAS_HEIGHT = 620;
+  /**
+   * キーが押されている状態を調べるためのオブジェクト
+   * このオブジェクトをプロジェクトのどこからでも参照したいので、グローバルオブジェクトのプロパティとして設定する
+   * @global
+   * @type {object}
+   */
+  window.isKeyDown = {};
 
-    /**
-     * Canvas2D APIをラップしたユーティリティクラス
-     * @type {Canvas2DUtility}
-     */
-    let util = null;
-    /**
-     * Canvas2D APIのコンテキスト
-     * @type {CanvasRenderingContext2D}}
-     */
-    let ctx = null;
-    /**
-     * 画像のインスタンス
-     *
-     */
+  /**
+   * canvasの幅
+   * @type {number}
+   */
+  const CANVAS_WIDTH = 400;
+  /**
+   * canvasの高さ
+   * @type {number}
+   */
+  const CANVAS_HEIGHT = 620;
 
+  /**
+   * Canvas2D APIをラップしたユーティリティクラス
+   * @type {Canvas2DUtility}
+   */
+  let util = null;
+  /**
+   * Canvas2D APIのコンテキスト
+   * @type {CanvasRenderingContext2D}}
+   */
+  let ctx = null;
+  /**
+   * 画像のインスタンス
+   *
+   */
 
-
-
-    let image = null;
-    /**
-     */
-    // 実行開始時のタイスタンプ
-    let startTime = null;
-    let comingStart = null;
-    /**
-     *
-     *
-     *
-     *
-     */
-    let viper = null;
-    /**
-     * リソースがすべて読み込まれたら実行されるアクション
-     */
-    window.addEventListener('load',
+  let image = null;
+  /**
+   */
+  // 実行開始時のタイスタンプ
+  let startTime = null;
+  let comingStart = null;
+  /**
+   *
+   *
+   *
+   *
+   */
+  let viper = null;
+  /**
+   * リソースがすべて読み込まれたら実行されるアクション
+   */
+  window.addEventListener(
+    "load",
     () => {
-        // ユーティリティクラスを使えるために準備する
-        util = new Canvas2DUtility(document.querySelector('#main_canvas'));
-        // ユーティリティクラスからHTMLcanvasElementへの参照を取得
-        canvas = util.canvas;
-        // 2Dレンダリングコンテキストを取得
-        ctx = util.context;
+      // ユーティリティクラスを使えるために準備する
+      util = new Canvas2DUtility(document.querySelector("#main_canvas"));
+      // ユーティリティクラスからHTMLcanvasElementへの参照を取得
+      canvas = util.canvas;
+      // 2Dレンダリングコンテキストを取得
+      ctx = util.context;
 
-        // まず最初に画像の読み込みを開始
-        util.imageLoader('./image/viper.png', loadedImage => {
-            // 引数経由で画像を受け取り変数に入れておく
+      // まず最初に画像の読み込みを開始
+      util.imageLoader("./image/viper.png", (loadedImage) => {
+        // 引数経由で画像を受け取り変数に入れておく
 
-            image = loadedImage;
-            //
-            initialize();
-            //
-            eventSetting();
-            //
-            startTime = Date.now();//now()はDateオブジェクトの静的メソッド
-            //
-            render();
-        });
-        },
-    false
-    );
-
-    /**
-     * canvasや２Dレンダリングコンテキストを初期化する
-     */
-    function initialize() {
-        canvas.window = CANVAS_WIDTH;
-        canvas.height = CANVAS_HEIGHT;
-
-        viper = new Viper(ctx, 0 ,0, 64, 64, image);
-        // 登場シーンから開始させる
-        viper.setComing(
-          CANVAS_WIDTH / 2,
-          CANVAS_HEIGHT + 50,
-          CANVAS_WIDTH / 2,
-          CANVAS_HEIGHT - 100,
-        );
-    }
-    function eventSetting() {
-        // キーが押されたときに、実行したいアクションを登録
-        window.addEventListener('keydown', e => {
-            // 登場シーン中はキーの入力を無視する
-            if (viper.isComing === true) {
-                return;
-            }
-            switch (e.key) {
-                case 'ArrowLeft':
-                    viper.position.x -= 10;
-                    break;
-                case 'ArrowRight':
-                    viper.position.x += 10;
-                    break;
-                case 'ArrowUp':
-                    viper.position.y -= 10;
-                    break;
-                case 'ArrowDown':
-                    viper.position.y += 10;
-                    break;
-            }
-        },false);
-    }
-    /**
-     *
-     */
-    function render() {
-        // キャンバス描画を完全な不透明にする
-        ctx.globalAlpha = 1.0;
+        image = loadedImage;
         //
-        util.drawRect(0, 0, canvas.width, canvas.height, 'black');
+        initialize();
+        //
+        eventSetting();
+        //
+        startTime = Date.now(); //now()はDateオブジェクトの静的メソッド
+        //
+        render();
+      });
+    },
+    false
+  );
 
+  /**
+   * canvasや２Dレンダリングコンテキストを初期化する
+   */
+  function initialize() {
+    canvas.window = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
 
+    viper = new Viper(ctx, 0, 0, 64, 64, image);
+    // 登場シーンから開始させる
+    viper.setComing(
+      CANVAS_WIDTH / 2,
+      CANVAS_HEIGHT + 50,
+      CANVAS_WIDTH / 2,
+      CANVAS_HEIGHT - 100
+    );
+  }
 
+  function eventSetting() {
+    // キーが押されたときに、実行したいアクションを登録
+    window.addEventListener(
+      "keydown",
+      (e) => {
+        // キーが押されたことを管理しているグローバルオブジェクトにキーが押されたという状態をプロパティとしてもたせる
+        isKeyDown[`key_${e.key}`] = true;
+      },
+      false
+    );
+    // キーが離されたときに、実行されるアクションを設定
+    window.addEventListener(
+      "keyup",
+      (e) => {
+        // キーが離されたという状態をもたせる
+        isKeyDown[`key_${e.key}`] = false;
+      },
+      false
+    );
+  }
+  /**
+   *
+   */
+  function render() {
+    // キャンバス描画を完全な不透明にする
+    ctx.globalAlpha = 1.0;
+    //
+    util.drawRect(0, 0, canvas.width, canvas.height, "black");
 
+    // 現在までの経過時間を取得
+    let nowTime = (Date.now() - startTime) / 1000;
 
+    // viperを更新して描画
+    viper.update();
+    // ずっとループさせつづけるために、描画処理を再帰呼び出し
+    requestAnimationFrame(render);
+  }
 
-
-
-
-
-
-        // 現在までの経過時間を取得
-        let nowTime = (Date.now() - startTime) / 1000;
-
-        // viperを更新して描画
-        viper.update();
-        // ずっとループさせつづけるために、描画処理を再帰呼び出し
-        requestAnimationFrame(render);
-    }
-
-    /**
-     *
-     *
-     */
-    function generateRandomInt(range) {
-        let random = Math.random();
-        return Math.floor(random * range);
-    }
+  /**
+   *
+   *
+   */
+  function generateRandomInt(range) {
+    let random = Math.random();
+    return Math.floor(random * range);
+  }
 })();
